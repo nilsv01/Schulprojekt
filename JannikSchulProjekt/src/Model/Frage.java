dd150495269768e798d11e43319c90f9349fa8a2
Jxnnx <Jxnnx@PC-Jannik.fritz.box>
2021-02-26 18:08:39 +0100

    Frage

--- JannikSchulProjekt/src/Model/Frage.java
+++ JannikSchulProjekt/src/Model/Frage.java
@@ -1,5 +1,38 @@
 package Model;
 
 public class Frage {
+	private String frage;
+	private String richtigeAntwort;
+	private String[] falscheAntworten;
+	
+	public Frage(String pFrage, String pAntwort, String[] pFalscheAntworten) {
+		frage= pFrage;
+		richtigeAntwort= pAntwort;
+		falscheAntworten=pFalscheAntworten;
+	}
 
+	public String getFrage() {
+		return frage;
+	}
+
+	public void setFrage(String frage) {
+		this.frage = frage;
+	}
+
+	public String getRichtigeAntwort() {
+		return richtigeAntwort;
+	}
+
+	public void setRichtigeAntwort(String richtigeAntwort) {
+		this.richtigeAntwort = richtigeAntwort;
+	}
+
+	public String[] getFalscheAntworten() {
+		return falscheAntworten;
+	}
+
+	public void setFalscheAntworten(String[] falscheAntworten) {
+		this.falscheAntworten = falscheAntworten;
+	}
+	
 }
