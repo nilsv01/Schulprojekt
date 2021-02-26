87799b831548e1ae6ce511a001cd10106f51af34
Techn <Techn@DESKTOP-J5E5ECI.speedport.ip>
2021-02-26 17:58:23 +0100

    Main Controller

--- JannikSchulProjekt/src/Controller/Controller.java
+++ JannikSchulProjekt/src/Controller/Controller.java
@@ -1,5 +1,12 @@
 package Controller;
 
 public class Controller {
-
+	private static Controller instance;
+	
+	public static Controller getInstance() {
+		if (Controller.instance == null) {
+			Controller.instance = new Controller();			
+		}
+		return Controller.instance;
+	}
 }
